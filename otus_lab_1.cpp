fa9b8f307f5151a5f729b7e532915da503de3649
AlexandrBashkirev <alexandr.bashkirev@gmail.com>
2019-04-07 13:44:31 +0300

    lets get started

---
+++ otus_lab_1.cpp
@@ -0,0 +1,10 @@
+﻿// otus_lab_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "lib.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "build " << version() << "\n";
+    std::cout << "Hello World!\n"; 
+}
